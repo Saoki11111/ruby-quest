5d658c43dc1b1de76b278367e49fde22f2e65d3d
Saoki11111 <t.aoki11111@gmail.com>
2020-03-23 00:36:08 +0900

    2-4 Monster クラスの初期値の設定

--- main.rb
+++ main.rb
@@ -17,9 +17,18 @@ class Monster
   # 値の取り出し、書き込みが可能
   attr_accessor :hp
 
+  # **params でハッシュ形式のみしか受け付けないようにする
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+# ハッシュ形式なのでどんなデータか把握しやすくなる
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 # ヒアドキュメントで出力
 puts <<~TEXT
