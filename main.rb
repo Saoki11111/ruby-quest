dcb63027559311631f260f0658e0effb600eeecc
Saoki11111 <t.aoki11111@gmail.com>
2020-03-22 21:48:30 +0900

    1-5 パラメータに任意の初期値を設定

--- main.rb
+++ main.rb
@@ -1,46 +1,30 @@
 class Brave
   # initializeメソッドを定義
-  def initialize
-    @name = 'テリー'
-    @hp = 500
-    @offense = 150
-    @defense = 100
+  # new 演算子から渡された引数を受け取る
+  # params で一括で受け取る
+  # 引数に**を記述:ハッシュしか受け取れなくなる
+  def initialize(**params)
+    # 各パラメータをハッシュで取得
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
   end
-
-  ## nameのセッター
-  #def name=(name)
-  #  @name = name
-  #end
 
   # nameのゲッター
   def name
     @name
   end
 
-  ## hpのセッター
-  #def hp=(hp)
-  #  @hp = hp
-  #end
-
   # hpのゲッター
   def hp
     @hp
   end
- 
-  ## offenseのセッター
-  #def offense=(offense)
-  #  @offense = offense
-  #end
 
   # offenseのゲッター
     def offense
       @offense
     end
-
-  ## defenseのセッター
-  #def defense=(defense)
-  #  @defense = defense
-  #end
 
   # defenseのゲッター
   def defense
@@ -50,20 +34,19 @@ class Brave
 end
 
 # 勇者クラスをインスタンス化
-brave = Brave.new
+## パラメータの値をinitialize に渡す
+# brave = Brave.new("テリー",500,150,100)
+#
+##パラメータの値をinitialize に渡す(パラメータを明示)
+# brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+#
+## new 演算子ごとに渡す引数が増える ため initialize で ハッシュ化
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 
-## パラメータに値を設定
-#brave.name = 'テリー'
-#brave.hp = 500
-#brave.offense = 150
-#brave.defense = 100
-# -> initialize method
-
-# 値を取り出す
-# puts "NAME:#{brave.name}"
-# puts "HP:#{brave.hp}"
-# puts "OFFENSE:#{brave.offense}"
-# puts "DEFENSE:#{brave.defense}"
+# # 複数人の勇者を作成しても各勇者ごとに違ったパラメータを設定できるようになる。
+# brave = Brave.new("テリー", 500, 150, 100)
+# brave2= Brave.new("マロン", 200, 150, 100)
+# brave3= Brave.new("プリン", 100, 300, 100)
 
 # ヒアドキュメントで
 puts <<~TEXT
