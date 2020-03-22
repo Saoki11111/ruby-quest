9d21534376340a7e3dbd20139ac6862d715481b5
Saoki11111 <t.aoki11111@gmail.com>
2020-03-23 00:32:08 +0900

    2-2 Monster クラスの定義

--- main.rb
+++ main.rb
@@ -1,29 +1,21 @@
 class Brave
-  # # attr_reader の記述でゲッターを省略できる(ゲッターの役割になる)
-  # attr_reader :name, :hp, offense
-  # attr_reader :hp
-  # attr_reader :offense
-  # attr_reader :defense
-  # # 複数同時に指定できる。
   attr_reader :name, :offense, :defense
-  ## attr_writer で セッターを定義
-  # attr_writer :hp
-  # セッターゲッターを一括定義
   attr_accessor :hp
 
   def initialize(**params)
-    # 各パラメータをハッシュで取得
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
   end
 
-  ## セッターを再定義
-  ## 短くかける ↑
-  #def hp=(hp)
-  #  @hp = hp
-  #end
+end
+
+class Monster
+  # 値の取り出し
+  attr_reader :name, :offense, :defense
+  # 値の取り出し、書き込みが可能
+  attr_accessor :hp
 
 end
 
