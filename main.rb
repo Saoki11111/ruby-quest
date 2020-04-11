75de4f996f773e4be369b3620db3f82adfe71200
Saoki11111 <t.aoki11111@gmail.com>
2020-04-11 22:01:04 +0900

    親クラスを追加

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 require 'pry'
 
-class Brave
+# 親クラス
+class Character
   attr_reader :offense, :defense
   attr_accessor :hp, :name
 
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
 
 class Brave
   attr_reader :name, :offense, :defense
