38fb5ff39b0553c2a04f8bca52dcb92bb8a5507b
Saoki11111 <t.aoki11111@gmail.com>
2020-04-11 19:59:02 +0900

    リファクタ loop処理(times) の追加 loop だと無限ループなので変更

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Brave
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
 
   # 必殺攻撃の計算に使う定数
   SPECIAL_ATTACK_CONSTANT = 1.5
@@ -18,11 +18,9 @@ class Brave
   def attack(monster)
     puts "#{@name}の攻撃"
 
-    # decision_attack_typeメソッドの呼び出し type に入れる
     attack_type = decision_attack_type
 
     damage = calculate_damage(target: monster, attack_type: attack_type)
-
     cause_damage(target: monster, damage: damage)
 
     puts "#{monster.name}の残り HPは #{monster.hp}だ"
@@ -35,27 +33,31 @@ class Brave
     attack_num = rand(4)
 
     if attack_num == 0
+      puts '必殺攻撃'
       'special_attack'
     else
+      puts '通常攻撃'
       'normal_attack'
     end
   end
 
   def calculate_damage(**params)
-
     target = params[:target]
     attack_type = params[:attack_type]
 
     if attack_type == "special_attack"
-      calculate_special_attack - monster.defense
+      calculate_special_attack - target.defense
     else
-      @offense - monster.defense
+      @offense - target.defense
     end
   end
 
-  def cause_damage(monster, damage)
-    monster.hp -= damage
-    puts "#{monster.name}は#{damage}のダメージを受けた"
+  def cause_damage(**params)
+    damage = params[:damage]
+    target = params[:target]
+
+    target.hp -= damage
+    puts "#{target.name}は#{damage}のダメージを受けた"
   end
 
   def calculate_special_attack
@@ -65,7 +67,7 @@ class Brave
 end
 
 class Monster
-  attr_reader :name, :offense, :defense
+  attr_reader :offense, :defense
   attr_accessor :hp, :name
 
   POWER_UP = 1.5
@@ -77,9 +79,8 @@ class Monster
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
-    @transformation = false
 
-    # @transformation_hp = params[:hp] / 2
+    @transformation = false
     @transformation_hp = params[:hp] * HELP_HP
   end
 
@@ -91,24 +92,24 @@ class Monster
     
     puts "#{@name}の攻撃"
 
-    # ダメージ計算処理の呼び出し
-    damage = caluclate_damage(target: brave, damage: damage) 
-
-    brave.hp -= damage
+    damage = calculate_damage(brave)
+    cause_damage(target: brave, damage: damage)
 
     puts "#{brave.name}の残りHPは#{brave.hp}だ"
   end
 
   private
-
-    # ダメージ計算処理
+    
     def calculate_damage(target)
-      # 引数で受け取った値を変数に格納
+      @offense - target.defense
+    end
+
+    def cause_damage(**params)
       damage = params[:damage]
-      damage = params[:target]
+      target = params[:target]
 
       target.hp -= damage
-      puts "#{brave.name}は #{damage} のダメージを受けた"
+      puts "#{target.name}は#{damage} のダメージを受けた"
     end
 
     def transform
@@ -116,7 +117,7 @@ class Monster
 
       puts <<~EOS
       #{@name}は怒っている
-      #{@name}は#{@transform_name}に変身した
+      #{@name}は#{transform_name}に変身した
       EOS
 
       @offense *= POWER_UP
@@ -130,5 +131,7 @@ end
 brave = Brave.new(name: "テリー", hp: 500, offense: 200, defense: 120)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 130)
 
-brave.attack(monster)
-monster.attack(brave)
+5.times do
+  brave.attack(monster)
+  monster.attack(brave)
+end
