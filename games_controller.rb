ccdc416fbfcc1e866a6f034ca848449b81c12234
Saoki11111 <t.aoki11111@gmail.com>
2020-04-12 02:12:01 +0900

    バトル終了時の処理を追加

--- games_controller.rb
+++ games_controller.rb
@@ -1,23 +1,24 @@
+require './message_dialog'
+
 class GamesController
+
+  include MessageDialog
+
   EXP_CONSTANT = 2
   GOLD_CONSTANT = 3
 
   # バトルの処理
   def battle(**params)
-    # build_characters を呼び出し
     build_characters(params)
 
     loop do
-      # インスタンス変数に変更
       @brave.attack(@monster)
       break if battle_end? 
 
-      # インスタンス変数に変更
       @monster.attack(@brave)
       break if battle_end? 
     end
 
-    # battle_judgement メソッドを呼び出す
     battle_judgement
   end
   
@@ -33,31 +34,27 @@ class GamesController
     end
 
 
-    # brave_win? にメソッド名を変更
     def brave_win?
       @brave.hp > 0
     end
 
-    # 勇者の勝敗によりメッセージを変更するメソッド
     def battle_judgement
-      # brave_win? を呼び出す
-      if brave_win?
-        result = calculate_of_exp_and_gold 
+      result = calculate_of_exp_and_gold 
 
-
-        # puts "#{@brave.name}はたたかいに勝った"
-        # puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールを獲得した"
-      else
-        # puts "#{@brave.name}はたたかいに負けた"
-        # puts "目の前が真っ暗になった"
-      end
+      end_message(result)
     end
 
     def calculate_of_exp_and_gold
-      exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
-      gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
-      result = {exp: exp, gold: gold}
+      if brave_win?
+        brave_win_flag = true
+        exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+        gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
+      else
+        brave_flag = false
+        exp = 0
+        gold = 0
+      end
 
-      result
+      {brave_win_flag: brave_win_flag, exp: exp, gold: gold}
     end
 end
